ac7cba671ad5cd353ac253de3499cab9884a6cd6
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-23 09:54:08 +0100

    Build Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="../public/logo.png" alt="Robot chef wearing a hat" className="logo" />
+            <h1 className="title">Chef Claude</h1>
+        </header>
+    );
+}
