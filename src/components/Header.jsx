4d9274c89b4dab0a98cdceba81a223daf9c0ee5e
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-23 09:58:36 +0100

    Import image to follow best practice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import chefClaudeLogo from "/logo.png";
+
 export default function Header() {
     return (
         <header className="header">
-            <img src="../public/logo.png" alt="Robot chef wearing a hat" className="logo" />
+            <img src={chefClaudeLogo} alt="Robot chef wearing a hat" className="logo" />
             <h1 className="title">Chef Claude</h1>
         </header>
     );
